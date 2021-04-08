e392d76580efb28a861b902c8c40229515dd9056
abdulvahap önen <82175373+abdulvahaponen@users.noreply.github.com>
2021-04-08 23:42:08 +0300

    Add files via upload

---
+++ MathClass/src/MathRepository.java
@@ -0,0 +1,16 @@
+import com.sun.xml.internal.ws.message.MimeAttachmentSet;
+
+public class MathRepository {
+public static void main(String[] args) {
+	System.out.println("MAth.ceil(2.6="+Math.ceil(2.6));
+	System.out.println("Math.cos(Math.PI):"+Math.cos(Math.PI));
+	System.out.println("MAth.cos(Math.PI)/2:"+Math.cos(Math.PI/2));
+	System.out.println("Math.Floor(2.6):"+Math.floor(2.6));
+	System.out.println("Math.Max(2.5,9:"+Math.max(2.5, 9));
+	System.out.println("MAth.Min(2.5,9:"+Math.min(2.5, 9));
+	System.out.println("MAth.pow(3,5):"+Math.pow(3,5));
+	System.out.println("Math.random:"+Math.random());
+	System.out.println("MAth.ceil(2.4):"+Math.ceil(2.4));
+	System.out.println("MAth.roud(2.6)="+Math.ceil(2.6));
+}
+}
